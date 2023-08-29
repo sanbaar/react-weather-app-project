ec7f2f89ae092f937b75f97b1f3138af8752bf0d
sanbaar <sandra.baar88@gmail.com>
2023-08-29 19:47:43 +0200

    Added html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App container">
       Weather App
+      <Weather />
       <footer>
         <a
           href="https://github.com/sanbaar/react-weather-app-project.git"
