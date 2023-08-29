3e14db08e653d09833f8ce01169578737fd8765e
sanbaar <sandra.baar88@gmail.com>
2023-08-29 19:14:43 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <a
+          href="https://github.com/sanbaar/react-weather-app-project.git"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by Sandra B.A.
+      </footer>
+    </div>
+  );
 }
