871508657bf5715c805df234290bc0469c288977
sanbaar <sandra.baar88@gmail.com>
2023-08-29 19:30:47 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App container">
       Weather App
       <footer>
         <a
           href="https://github.com/sanbaar/react-weather-app-project.git"
           target="_blank"
+          rel="noreferrer"
         >
           Open-source code
         </a>{" "}
