ff2e936e517f1f85496f787ff3e65bd5568833a6
sanbaar <sandra.baar88@gmail.com>
2023-08-30 19:56:14 +0200

    added more js

--- src/Weather.js
+++ src/Weather.js
@@ -3,14 +3,20 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
-    setTemperature(Math.round(response.data.main.temp));
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      city: response.data.name,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+    });
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <body>
         {" "}
@@ -26,9 +32,11 @@ export default function Weather() {
               <input type="submit" value="Search" id="search-button" />
             </form>
 
-            <h1></h1>
+            <h1>{weatherData.city}</h1>
             <div className="current-date" id="date"></div>
-            <div className="description"></div>
+            <div className="description text-capitalize">
+              {weatherData.description}
+            </div>
             <div className="row current-day-parameters">
               <div className="col-3">
                 <img src="" alt="" id="icon" />
@@ -38,7 +46,7 @@ export default function Weather() {
                   className="fa-solid fa-temperature-quarter trw-icon"
                   alt="Current temperature"
                 ></i>
-                <span id="main-temperature"> </span>
+                <span id="main-temperature"> {weatherData.temperature} </span>
 
                 <span className="trw-units">ºC </span>
               </div>
@@ -47,12 +55,12 @@ export default function Weather() {
                   className="fa-light fa-droplet-percent trw-icon"
                   alt="humidity"
                 ></i>
-                <span id="main-humidity"></span>
+                <span id="main-humidity">{weatherData.humidity}</span>
                 <span class="trw-units">% RH</span>
               </div>
               <div className="col-3 temp-rain-wind">
                 <i className="fa-solid fa-wind trw-icon" alt="Wind speed"></i>
-                <span id="main-wind"></span>
+                <span id="main-wind">{weatherData.wind}</span>
                 <span className="trw-units">m/s</span>
               </div>
             </div>
@@ -61,15 +69,15 @@ export default function Weather() {
           </div>
         </div>
         <div className="open-source-code">
-          This project was coded by Sandra Bagés-Arnal and is
+          This project was coded by Sandra Bagés-Arnal and it is{" "}
           <a
             href="https://github.com/sanbaar/weather-app"
             target="_blank"
             rel="noreferrer"
           >
             open-sourced on GitHub
-          </a>
-          and
+          </a>{" "}
+          and{" "}
           <a
             href="https://euphonious-sundae-c00b2a.netlify.app/"
             target="_blank"
@@ -82,7 +90,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "93d43dfe3b4a950e5b187e5dc313705e";
-    let city = "Barcelona";
+    const city = "Barcelona";
     let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiURL).then(handleResponse);
 
