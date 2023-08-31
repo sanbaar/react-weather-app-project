aac3cf3d49bae0ffef488f9df34d8b2dd0acd0e9
sanbaar <sandra.baar88@gmail.com>
2023-08-31 19:35:33 +0200

    Created WeatherInfo component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
-import "./Weather.css";
 import React, { useState } from "react";
 import axios from "axios";
-import FormatedDate from "./FormatedDate";
+import WeatherInfo from "./WeatherInfo";
+
+import "./Weather.css";
 
 export default function Weather() {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -21,7 +22,6 @@ export default function Weather() {
   if (weatherData.ready) {
     return (
       <body>
-        {" "}
         <div className="card">
           <div className="card-body">
             <form id="search-city">
@@ -33,63 +33,9 @@ export default function Weather() {
               />
               <input type="submit" value="Search" id="search-button" />
             </form>
-
-            <h1>{weatherData.city}</h1>
-            <div className="current-date" id="date">
-              <FormatedDate date={weatherData.date} />
-            </div>
-            <div className="description text-capitalize">
-              {weatherData.description}
-            </div>
-            <div className="row current-day-parameters">
-              <div className="col-3">
-                <img src="" alt="" id="icon" />
-              </div>
-              <div className="col-3 temp-rain-wind">
-                <i
-                  className="fa-solid fa-temperature-quarter trw-icon"
-                  alt="Current temperature"
-                ></i>
-                <span id="main-temperature"> {weatherData.temperature} </span>
-
-                <span className="trw-units">ºC </span>
-              </div>
-              <div className="col-3 temp-rain-wind">
-                <i
-                  className="fa-light fa-droplet-percent trw-icon"
-                  alt="humidity"
-                ></i>
-                <span id="main-humidity">{weatherData.humidity}</span>
-                <span class="trw-units">% RH</span>
-              </div>
-              <div className="col-3 temp-rain-wind">
-                <i className="fa-solid fa-wind trw-icon" alt="Wind speed"></i>
-                <span id="main-wind">{weatherData.wind}</span>
-                <span className="trw-units">m/s</span>
-              </div>
-            </div>
-
-            <div className="weather-forecast"></div>
+            <WeatherInfo data={weatherData} />
           </div>
         </div>
-        <div className="open-source-code">
-          This project was coded by Sandra Bagés-Arnal and it is{" "}
-          <a
-            href="https://github.com/sanbaar/weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://euphonious-sundae-c00b2a.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </div>{" "}
       </body>
     );
   } else {
---
+++ src/WeatherInfo.js
@@ -0,0 +1,63 @@
+import React from "react";
+import FormatedDate from "./FormatedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <div className="current-date" id="date">
+        <FormatedDate date={props.data.date} />
+      </div>
+      <div className="description text-capitalize">
+        {props.data.description}
+      </div>
+      <div className="row current-day-parameters">
+        <div className="col-3">
+          <img src="" alt="" id="icon" />
+        </div>
+        <div className="col-3 temp-rain-wind">
+          <i
+            className="fa-solid fa-temperature-quarter trw-icon"
+            alt="Current temperature"
+          ></i>
+          <span id="main-temperature"> {props.data.temperature} </span>
+
+          <span className="trw-units">ºC </span>
+        </div>
+        <div className="col-3 temp-rain-wind">
+          <i
+            className="fa-light fa-droplet-percent trw-icon"
+            alt="humidity"
+          ></i>
+          <span id="main-humidity">{props.data.humidity}</span>
+          <span class="trw-units">% RH</span>
+        </div>
+        <div className="col-3 temp-rain-wind">
+          <i className="fa-solid fa-wind trw-icon" alt="Wind speed"></i>
+          <span id="main-wind">{props.data.wind}</span>
+          <span className="trw-units">m/s</span>
+        </div>
+
+        <div className="weather-forecast"></div>
+      </div>
+      <div className="open-source-code">
+        This project was coded by Sandra Bagés-Arnal and it is{" "}
+        <a
+          href="https://github.com/sanbaar/weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://euphonious-sundae-c00b2a.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </div>
+    </div>
+  );
+}
